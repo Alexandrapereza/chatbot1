9b114e53b54aeed9448e86d6c0250c91f3a8f406
Alexandrapereza <161147045+Alexandrapereza@users.noreply.github.com>
2024-02-24 14:57:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from openai import OpenAI 
 
-client = OpenAI(api_key = st.secrets.Openai.API)
+client = OpenAI(api_key = st.secrets.Openai.apikey)
 
 
 if "messages" not in st.session_state:
